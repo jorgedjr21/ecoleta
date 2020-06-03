fa066038ce5c553419af8de05c14cbe3fcf6c4cb
Jorge Junior <jorgedjr21@gmail.com>
2020-06-03 17:00:35 -0300

    extract routes to new file

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,36 +1,9 @@
 import express from 'express';
+import routes from './routes';
 
 const app  = express();
 app.use(express.json());
+app.use(routes);
 
-const users  = [
-  'Jorge',
-  'Jose',
-  'Carlos',
-  'Joao'
-];
-
-app.get('/users', (req, res) => {
-  const search = String(req.query.search);
-  const filteredUsers = search ? users.filter(user => user.includes(search)) : users
-
-  return res.json(filteredUsers);
-});
-
-app.get('/users/:id', (req, res) => {
-  const id = Number(req.params.id);
-  return res.json(users[id]);
-});
-
-app.post('/users', (req, res) => {
-  const data = req.body;
-
-  const user = {
-    name: data.name,
-    email: data.email
-  };
-
-  return res.json(user)
-})
 
 app.listen(3333);
