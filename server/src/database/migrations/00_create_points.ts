110725a58370e4813b5a6515cba069d70a8736e4
Jorge Junior <jorgedjr21@gmail.com>
2020-06-03 20:12:42 -0300

    update migration to install uuid extension

--- server/src/database/migrations/00_create_points.ts
+++ server/src/database/migrations/00_create_points.ts
@@ -2,16 +2,18 @@ import Knex from 'knex';
 
 export async function up(knex: Knex) {
   
-  return knex.schema.createTable('points', table => {
-    table.uuid('id').primary().defaultTo(knex.raw('uuid_generate_v4()'));
-    table.string('image').notNullable();
-    table.string('name').notNullable();
-    table.string('email').notNullable();
-    table.string('whatsapp').notNullable();
-    table.decimal('latitude').notNullable();
-    table.decimal('longitude').notNullable();
-    table.string('city').notNullable();
-    table.string('uf', 2).notNullable();
+  return knex.schema
+    .raw('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"')
+    .createTable('points', table => {
+      table.uuid('id').primary().defaultTo(knex.raw('uuid_generate_v4()'));
+      table.string('image').notNullable();
+      table.string('name').notNullable();
+      table.string('email').notNullable();
+      table.string('whatsapp').notNullable();
+      table.decimal('latitude').notNullable();
+      table.decimal('longitude').notNullable();
+      table.string('city').notNullable();
+      table.string('uf', 2).notNullable();
 
   })
 }
