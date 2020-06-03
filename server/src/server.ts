cb8672e1232004cf30cad0bb422592fee76fc0a8
Jorge Junior <jorgedjr21@gmail.com>
2020-06-03 16:56:55 -0300

    aula 02 - metodos http

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,16 +1,36 @@
 import express from 'express';
 
 const app  = express();
+app.use(express.json());
 
-app.get('/users', (request, response) => {
-  console.log('Listagem de usuários');
+const users  = [
+  'Jorge',
+  'Jose',
+  'Carlos',
+  'Joao'
+];
 
-  response.json([
-    'Jorge',
-    'Jose',
-    'Carlos',
-    'Joao'
-  ]);
+app.get('/users', (req, res) => {
+  const search = String(req.query.search);
+  const filteredUsers = search ? users.filter(user => user.includes(search)) : users
+
+  return res.json(filteredUsers);
 });
+
+app.get('/users/:id', (req, res) => {
+  const id = Number(req.params.id);
+  return res.json(users[id]);
+});
+
+app.post('/users', (req, res) => {
+  const data = req.body;
+
+  const user = {
+    name: data.name,
+    email: data.email
+  };
+
+  return res.json(user)
+})
 
 app.listen(3333);
